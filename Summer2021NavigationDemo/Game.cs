c56346c9592050be09f4e388a5071bf67387a103
Janell Baxter <jbaxter@colum.edu>
2021-07-20 13:40:32 -0500

    Add project files.

---
+++ Summer2021NavigationDemo/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summer2021NavigationDemo
+{
+    public class Game
+    {
+        //One class can contain instances of another class
+        //This is called Containment - the relationship is informally 
+        //known as "has a"
+
+        public string Name = "Super Awesome Game";
+        //A Game has multiple Areas
+        public List<Area> Areas = new List<Area>();
+        //Game has a Player
+        public Person Player = new Person();
+
+        //other 2 important relationships
+        //"is a" - inheritance
+        //"uses a" - general association
+
+    }
+}
---
+++ Summer2021NavigationDemo/Start.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Summer2021NavigationDemo
+{
+    /// <summary>
+    /// Interaction logic for Start.xaml
+    /// </summary>
+    public partial class Start : Page
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Map.xaml", UriKind.Relative));
+            if (PlayerNameInput.Text != "")
+            {
+                MainWindow.game.Player.Name = PlayerNameInput.Text;
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Data.Text = $"Welcome to {MainWindow.game.Name} {MainWindow.game.Player.Name}.\nEnter the name you wish to be known by below\nthen click the button to start your adventure!";
+        }
+    }
+}
